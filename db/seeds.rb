b359b3bbaa72ed300fd6b8e3b151039538535145
anuarustayev <anuar.ustayev@gmail.com>
2016-07-06 12:50:27 +0600

    seed file, modals fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,14 @@
 
 Seller.destroy_all
 User.destroy_all
+Pickup.destroy_all
+
+Seller.create! [
+  { seller_name: "4kinder", seller_rep_name: "Ermek Abikenov", seller_address1: "Makatayev 117", seller_phone: "87055551234", email: "test@test.com", password: "abc123" },
+  { seller_name: "kaspi shop", seller_rep_name: "Kim", seller_address1: "Nauryzbai Batyr", seller_phone: "87055551234", email: "test1@test.com", password: "abc123" }
+]
+
+Pickup.create! [
+  { first_name: "Test", last_name: "Testov", company: "Duken", email: "pick@pick.com", password: "abc123" },
+  { first_name: "Test1", last_name: "Testov1", company: "Duken1", email: "pick1@pick.com", password: "abc123" }
+]
