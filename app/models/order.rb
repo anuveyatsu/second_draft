f7a1398bc7c9b40fb1c6850b109f33b4d6582651
anuarustayev <anuar.ustayev@gmail.com>
2016-07-13 15:22:11 +0600

    landing page layout improved, content changed, fonts added

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,9 @@ class Order < ActiveRecord::Base
   belongs_to :warehouse
   has_many :travel_standards
   has_many :travel_premia
+  after_initialize :init
+
+  def init
+    self.delivery_status ||= "processing" #will set the default value only if it's null
+  end
 end
