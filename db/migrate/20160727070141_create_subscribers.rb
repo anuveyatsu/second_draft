dacf9df488f1605827220111a0042cd316e8488b
anuarustayev <anuar.ustayev@gmail.com>
2016-07-29 15:11:22 +0600

    drop table removed

--- db/migrate/20160727070141_create_subscribers.rb
+++ db/migrate/20160727070141_create_subscribers.rb
@@ -1,6 +1,6 @@
 class CreateSubscribers < ActiveRecord::Migration
   def change
-    drop_table :subscribers
+    #drop_table :subscribers
     create_table :subscribers do |t|
       t.string :email
 
