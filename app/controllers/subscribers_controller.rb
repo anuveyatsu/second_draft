e82e32af7dcd24f299b0e252092fac3e2df47298
anuarustayev <anuar.ustayev@gmail.com>
2016-08-08 23:27:22 +0600

    1)subscruber email validation only front-end with javascript; 2)subscriver email input size optimized; 3)collapsable navbar behaviour fixed to hide when click on body element;

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,9 +3,12 @@ class SubscribersController < ApplicationController
     @subscriber = Subscriber.new(sub_params)
     @subscriber.save
     redirect_to root_path, notice: 'Thank you! We will be in touch with you.'
+
   end
+
   private
   def sub_params
     params.require(:subscriber).permit(:email)
   end
+
 end
